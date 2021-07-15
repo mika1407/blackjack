d513a9b927e1d2201e9e6f74cfe12f35ae5a92b1
mika1407 <koneautomaatio@hotmail.com>
2021-07-15 10:36:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let firstCard = getRandomInt()
+let secondCard = getRandomInt()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame(){
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else  {
+    message = "You're out of the game"
+    isAlive = false
+  }
+  messageEl.textContent = message
+  sumEl.textContent = "Sum: " + sum
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!")
+  let card = getRandomInt()
+  sum += card
+  renderGame()
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(1);
+  max = Math.floor(12);
+  return Math.floor(Math.random() * (12 - 1) + 1); //The maximum is exclusive and the minimum is inclusive
+}
+
+

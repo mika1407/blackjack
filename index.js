b69458b3d307613deb558acde5967cf857d26413
mika1407 <koneautomaatio@hotmail.com>
2021-07-16 11:52:33 +0300

    startGame fixed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+console.log(cards)
 
 function getRandomCard() {
   // if 1 (ace)  -> return 11
@@ -23,6 +23,11 @@ function getRandomCard() {
 }
 
 function startGame(){
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards =[firstCard, secondCard]
+  sum = firstCard + secondCard
   renderGame()
 }
 
